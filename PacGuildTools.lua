3017000f897607bbc50275724514cd7a67a92e74
Erica Zavaleta <micfeedbak@gmail.com>
2019-03-30 21:29:48 -0500

    Cleaned up Code into organized functions

--- PacGuildTools.lua
+++ PacGuildTools.lua
@@ -1,7 +1,7 @@
 
--- Required Libraries
+-- Load Required Libraries
 local LAM2 = LibStub:GetLibrary("LibAddonMenu-2.0")
-local LIBMW = LibStub:GetLibrary("LibMsgWin-1.0")
+--local LIBMW = LibStub:GetLibrary("LibMsgWin-1.0")
 
 -- Initialize our Namespace Table
 PacsAddon = {}
@@ -19,6 +19,7 @@ function PacsAddon:Initialize()
 
     enableDebug = PacsAddon.savedVariables.enableDebug
     activeGuild = PacsAddon.savedVariables.activeGuild
+    activeGuildID = PacsAddon.savedVariables.activeGuildID
 
     -- If this is the first run, or the saved settings file is missing lets set the first guild as the default
     if isempty(activeGuild) then
@@ -29,7 +30,7 @@ function PacsAddon:Initialize()
     -- Currently the Saved Settings saves the Guilds Name.  Lets grab the active guilds index ID.  
     for guildIndex = 1, 5 do
         if activeGuild == GetGuildName(guildIndex) then
-            activeGuildID = guildIndex
+            PacsAddon.savedVariables.activeGuildID = guildIndex
         end
     end
 
@@ -37,85 +38,25 @@ function PacsAddon:Initialize()
     guildName = GetGuildName(activeGuildID)
     guildMemberNum = GetNumGuildMembers(activeGuildID)
 
-    -- Grab the guild roster from the ESO API
-    masterList = {}
-    for guildMemberIndex = 1, guildMemberNum do
-        local displayName, note, rankIndex, status, secsSinceLogoff = GetGuildMemberInfo(activeGuildID, guildMemberIndex)
+    UpdateGuildRoster()
+    UpdateGuildHistory()
 
-        if status == 1 then
-            statusString = "Online"
-        elseif status == 2 then
-            statusString = "Away"
-        elseif status == 3 then
-            statusString = "Do Not Distrub"
-        elseif status == 4 then
-            statusString = "Offline"
-        end
-
-        local data = {
-                        index = guildMemberIndex,
-                        displayName = displayName,
-                        note = note,
-                        rankIndex = rankIndex,
-                        status = status,
-                        statusString = statusString,
-                        secsSinceLogoff = secsSinceLogoff,
-                        logoffString = SecondsToClock(secsSinceLogoff),
-                    }
-        masterList[guildMemberIndex] = data
-
-    end
-
-    local numGuildBankEvents = GetNumGuildEvents(activeGuildID, GUILD_HISTORY_BANK)
-    guildBankHistory = {}
-    for GuildBankEventsIndex = 1, numGuildBankEvents do
-        local eventType, secsSinceEvent, displayName, count, itemLink = GetGuildEventInfo(activeGuildID, GUILD_HISTORY_BANK, GuildBankEventsIndex)
-
-        if eventType == 21 then
-            eventName = "Bankgold Added"
-        elseif eventType == 22 then
-            eventName = "Bankgold Removed"
-        elseif eventType == 14 then
-            eventName = "Bankitem Removed"
-        elseif eventType == 13 then
-            eventName = "Bankitem Added"
-        else
-            eventName = "Unknown"
-        end
-
-        local data = {
-                    eventName = eventName,
-                    eventType = eventType,
-                    secsSinceEvent = secsSinceEvent,
-                    timestamp = os.date("%m/%d/%Y %H:%M:%S %z", (os.time() - secsSinceEvent)),
-                    displayName = displayName,
-                    count = count,
-                    itemLink = itemLink,
-                    item = GetItemLinkName(itemLink)
-                }
-        guildBankHistory[GuildBankEventsIndex] = data
-    end
-
-
-    -- Save the Guild Roster to the saved settings file so we can convert it to CSV outside of ESO.  The saved settings file is only 
-    -- written to when logging out, or doing a /reloadui
-    PacsAddon.savedVariables.guildRoster = masterList
-    PacsAddon.savedVariables.guildDepositList = guildBankHistory
     PacsAddon.savedVariables.lastUpdate = time
 
-    -- local myMsgWindow = LIBMW:CreateMsgWindow("UberAddonChatWindow", "Window Header Label", 0, 0)
+    --local myMsgWindow = LIBMW:CreateMsgWindow("PacGuildTools", "Pacrooti's Guild Tools", 0, 0)
+    --myMsgWindow:AddText("Heres a chat message in red", 1, 0, 0)
 
     -- Debug output if we have that enabled. 
     if enableDebug == true then
-        d("PacGuildTools Initialized")
         d("Active Guild " .. activeGuild)
         d("Active Guild ID " .. activeGuildID)
+        d("PacGuildTools Init Finished")
         d(time)
-        d(numGuildBankEvents)
     end
 end
 
 
+-- Run when Addon Loads
 function PacsAddon.OnAddOnLoaded(event, addonName)
     -- The event fires each time *any* addon loads - but we only care about when our own addon loads.
     if addonName == PacsAddon.name then
@@ -178,15 +119,18 @@ function pgt_raffle(extra)
     end
 end
 
+
 -- Summon Pacrooti!!!
 function summon_pacrooti(extra)
     SetCrownCrateNPCVisible(true)
 end
 
+
 -- Dismiss Pacrooti
 function dismiss_pacrooti(extra)
     SetCrownCrateNPCVisible(false)
 end
+
 
 -- Convert Seconds to Hours, Min, Seconds
 function SecondsToClock(seconds)
@@ -202,15 +146,104 @@ function SecondsToClock(seconds)
     end
 end
 
+
 -- Check if a Variable is empty
 function isempty(s)
     return s == nil or s == ''
 end
 
 
+-- Update Guild Roster
+function UpdateGuildRoster(extra)
+    local activeGUildID = PacsAddon.savedVariables.activeGuildID
+    local enableDebug = PacsAddon.savedVariables.enableDebug
+    -- local guildName = GetGuildName(activeGuildID)
+    local guildMemberNum = GetNumGuildMembers(activeGuildID)
+
+    -- Grab the guild roster from the ESO API
+    local masterList = {}
+    for guildMemberIndex = 1, guildMemberNum do
+        local displayName, note, rankIndex, status, secsSinceLogoff = GetGuildMemberInfo(activeGuildID, guildMemberIndex)
+
+        if status == 1 then
+            statusString = "Online"
+        elseif status == 2 then
+            statusString = "Away"
+        elseif status == 3 then
+            statusString = "Do Not Distrub"
+        elseif status == 4 then
+            statusString = "Offline"
+        end
+
+        local data = {
+                        index = guildMemberIndex,
+                        displayName = displayName,
+                        note = note,
+                        rankIndex = rankIndex,
+                        rankName = GetGuildRankCustomName(activeGuildID, rankIndex),
+                        status = status,
+                        statusString = statusString,
+                        secsSinceLogoff = secsSinceLogoff,
+                        logoffString = SecondsToClock(secsSinceLogoff),
+                    }
+        masterList[guildMemberIndex] = data
+    end
+
+    PacsAddon.savedVariables.guildRoster = masterList
+
+    if enableDebug == true then
+        d("Updated Saved Var Guild roster with " .. guildMemberNum .. " members.")
+    end
+end
+
+
+-- Update Guild Bank History
+function UpdateGuildHistory(extra)
+    local activeGUildID = PacsAddon.savedVariables.activeGuildID
+    local enableDebug = PacsAddon.savedVariables.enableDebug
+
+    local numGuildBankEvents = GetNumGuildEvents(activeGuildID, GUILD_HISTORY_BANK)
+    local guildBankHistory = {}
+    for GuildBankEventsIndex = 1, numGuildBankEvents do
+        local eventType, secsSinceEvent, displayName, count, itemLink = GetGuildEventInfo(activeGuildID, GUILD_HISTORY_BANK, GuildBankEventsIndex)
+
+        if eventType == 21 then
+            eventName = "Bankgold Added"
+        elseif eventType == 22 then
+            eventName = "Bankgold Removed"
+        elseif eventType == 14 then
+            eventName = "Bankitem Removed"
+        elseif eventType == 13 then
+            eventName = "Bankitem Added"
+        else
+            eventName = "Unknown"
+        end
+
+        local data = {
+                    eventName = eventName,
+                    eventType = eventType,
+                    secsSinceEvent = secsSinceEvent,
+                    timestamp = os.date("%m/%d/%Y %H:%M:%S %z", (os.time() - secsSinceEvent)),
+                    displayName = displayName,
+                    count = count,
+                    itemLink = itemLink,
+                    item = GetItemLinkName(itemLink)
+                }
+        guildBankHistory[GuildBankEventsIndex] = data
+    end
+
+    PacsAddon.savedVariables.guildDepositList = guildBankHistory
+
+    if enableDebug == true then
+        d("Updated Saved Var Guild history with " .. numGuildBankEvents .. " events.")
+    end
+end
+
+
 -- function PacsAddonUpdate()
 --     PacAddonIndicatorCount:SetText("Test Text \n test text 2")
 -- end
+
 
 --  Settings Menu Function via LibAddonMenu-2.0
 function PacsAddon.CreateSettingsWindow()
